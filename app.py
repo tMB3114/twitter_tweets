a3ceaf6bbc6445c5d702ce343809dcc7f874eacd
tMB3114 <69705831+tMB3114@users.noreply.github.com>
2021-03-25 19:02:45 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-
 import numpy as np
 import tensorflow as tf
 import keras
@@ -30,11 +29,15 @@ a = np.array(pad_sequences(tweet_search,max_length))
 df = pd.DataFrame(a)
 
 # to filter array
+
+
+# to filter array
 def filter_array(arr):
     final_array = [] 
     for a in arr:
             if a!=0:
                 final_array.append(a)
+    return final_array            
     
 app = Flask(__name__)
 
@@ -44,49 +47,58 @@ def hello():
  
 @app.route('/', methods = ['POST','GET'])
 def my_for_post():
-     name = request.form['word']
-     user_input = name.lower()
-     word_array = user_input.split()
-     v = []
-     hiss = 0
-     for i in range(len(word_array)):
+    
+    
+    name = request.form['word']
+    if name == '':
+        return render_template("home.html", information = "Input cannot be empty !! ")
+           
+    user_input = str(name).lower()
+    word_array = user_input.split()
+    if 'mamba' in word_array:  
+        information = random_tweet(data)
+        return render_template('home.html', information = information)   
+    v = []
+    hiss = 0
+    for i in range(len(word_array)):
             val = word_index.get(word_array[i])
             v.append(val)    
             if not val:
                 hiss = 1  
-     if 'mamba' in word_array:  
-         information = random_tweet(data)
-         return render_template('home.html', information = information)        
-                
-     elif hiss == 1:
-         information = "Hello Kale Dai !! Please enter other Nepali words  "
-         return render_template('home.html', information = information)
-                      
-     else:
+       
+
+    if hiss == 1:
+        information = "Sorry !! Please Enter other Nepali words  "
+        return render_template('home.html', information = information)
+    if hiss == 0:
+        
+         
+   
+        
         #to shuffle row index   
         row_arr = np.arange(0,df.shape[0])
         np.random.shuffle(row_arr)
         df.index = row_arr
-        value = 0  
+        
         a = 0
         for ind in row_arr:  
+            value = 0
             for i in range(len(v)):
                 if v[i]  in df.values[ind]:
                     value = 1 + value
                 else:
                     value = 0 
-                    continue
+                    
      
             if value == len(v):
                 a = ind
-                break     
-    
-    
+                break
+                    
         if a == 0:
             information = "Enter next word"
-            return render_template('home.html', information = information)
             
-        else:            
+            return render_template('home.html', information = information) 
+        if a != 0:        
             arr = filter_array((df.iloc[a,:].values).tolist())
             final = ""
             word = ''
@@ -98,6 +110,11 @@ def my_for_post():
                 final = final + word + " " 
             information = final
             return render_template('home.html', information = information)
+
+        
+    
+           
+
         
     
            
